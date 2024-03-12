b6adcd82e95e46d92f8f8bef642daed1ee3ca551
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-12 14:57:08 +0500

    add file for saving other codes

---
+++ Whole-Code.c
@@ -0,0 +1,21 @@
+
+//C++ Tutorials:
+
+//Instructions for Running the Program:
+    //1. open terminal
+    //2. move to the location where the programs are located
+    //3. to compile the program, run the commmand: gcc filname.c
+    //4. to execute the program, run the command: ./a.exe if on windows and .\a.out if on mac or linux
+
+    
+
+//1. Hello World Program
+#include<stdio.h>
+
+int main() {
+    printf("Hello world");
+
+    return 0;
+}
+
+//Variables
