820798b3f6b2a286842037ed879ed96f77b91417
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-12 14:40:59 +0500

    add first c program: hello world

---
+++ Hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello world");
+
+    return 0;
+}
+
+//Instructions for Running the Program:
+    //1. open terminal
+    //2. move to the location where the programs are located
+    //3. to compile the program, run the commmand: gcc filname.c
+    //4. to execute the program, run the command: ./a.exe if on windows and .\a.out if on mac or linux
