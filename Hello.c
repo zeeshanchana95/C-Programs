43c530be4511d88e891825d9f704b7f72889ec35
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-13 21:13:13 +0500

    add a basic program and explain compilation process

--- Hello.c
+++ Hello.c
@@ -1,19 +1,26 @@
-//Input:
-//use "scanf()" to get input from user
-//we also have different cases for getting input of different types
-//Integer Format Specifier: scan("%d", &age); 
-//Real Number Format Specifier: scan("%f", &pi); 
-//Char Format Specifier: scan("%c", &star); 
-
+//Basic Sum Program:
 
 #include<stdio.h> 
 int main() {
 
-  int age;
-  printf("Enter age: ");
-  scanf("%d", &age);
-  printf("age is: %d\n", age);
+  int a,b;
+  printf("Enter a: ");
+  scanf("%d",&a);
+
+  printf("Enter b: ");
+  scanf("%d",&b);
+
+  // int sum = a + b;
+  printf("Addition of %d and %d is: %d\n", a, b, a+b);
+  printf("Subtraction of %d and %d is: %d\n", a, b, a-b);
+  printf("Multiplication of %d and %d is: %d\n", a, b, a*b);
+  printf("Division of %d and %d is: %d\n", a, b, a/b);
 
   return 0;
 }
 
+
+//Compilation:
+  //a computer program that translates C code into machine code (0's and 1's)
+  // C Program -> C Compiler -> Output File (a.exe or a.out)
+  // Hello.cpp -> gcc Hello.c -> a.exe 
--- Whole-Code.c
+++ Whole-Code.c
@@ -290,3 +290,57 @@ int main() {
   return 0;
 }
 
+
+
+
+
+
+//Basic Sum Program:
+
+#include<stdio.h> 
+int main() {
+
+  int a,b;
+  printf("Enter a: ");
+  scanf("%d",&a);
+
+  printf("Enter b: ");
+  scanf("%d",&b);
+
+  // int sum = a + b;
+  printf("Addition of %d and %d is: %d\n", a, b, a+b);
+  printf("Subtraction of %d and %d is: %d\n", a, b, a-b);
+  printf("Multiplication of %d and %d is: %d\n", a, b, a*b);
+  printf("Division of %d and %d is: %d\n", a, b, a/b);
+
+  return 0;
+}
+
+
+
+//Basic Sum Program:
+
+#include<stdio.h> 
+int main() {
+
+  int a,b;
+  printf("Enter a: ");
+  scanf("%d",&a);
+
+  printf("Enter b: ");
+  scanf("%d",&b);
+
+  // int sum = a + b;
+  printf("Addition of %d and %d is: %d\n", a, b, a+b);
+  printf("Subtraction of %d and %d is: %d\n", a, b, a-b);
+  printf("Multiplication of %d and %d is: %d\n", a, b, a*b);
+  printf("Division of %d and %d is: %d\n", a, b, a/b);
+
+  return 0;
+}
+
+
+//Compilation:
+  //a computer program that translates C code into machine code (0's and 1's)
+  // C Program -> C Compiler -> Output File (a.exe or a.out)
+  // Hello.cpp -> gcc Hello.c -> a.exe 
