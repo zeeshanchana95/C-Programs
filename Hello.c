5b4a8424cd09318c082c29bbe21dba90cecaad3c
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-13 20:56:24 +0500

    add code for getting input from user

--- Hello.c
+++ Hello.c
@@ -1,41 +1,18 @@
-//Output:
-//use printf() function to print something to screen
-//use printf("\n") function to print something to screen plus add newline at the end
+//Input:
+//use "scanf()" to get input from user
+//we also have different cases for getting input of different types
+//Integer Format Specifier: scan("%d", &age); 
+//Real Number Format Specifier: scan("%f", &pi); 
+//Char Format Specifier: scan("%c", &star); 
+
 
 #include<stdio.h> 
 int main() {
 
-  // printf("Hello World!\n");
-  // printf("Hello World!\n");
-  // printf("Hello World!\n");
-  // printf("Hello World!\n");
-
-
-  int age = 22;
-  printf("Age is age\n"); //Age is age
-
-  //There are some cases in C for Output:
-    //CASES
-      // 1 . integers (%d)
-      // printf(" age is %d ", age);
-
-      // 2. real numbers (%f)
-      // printf(" value of pi is %f ",
-      
-      // 3. characters (%c)
-      // printf("star looks like this %c star);
-
-      //So, basically we need to use "format specifiers" to get the value from the variable to be used inside printf() function
-
-      //printf() is the library function that comes with C library 
-
-      printf("Age is %d\n", age);
-
-      float pi = 3.14;
-      printf("PI is %f\n", pi);
-
-      char star = '*';
-      printf("I am star %c\n", star); 
+  int age;
+  printf("Enter age: ");
+  scanf("%d", &age);
+  printf("age is: %d\n", age);
 
   return 0;
 }
--- Whole-Code.c
+++ Whole-Code.c
@@ -267,3 +267,26 @@ int main() {
   return 0;
 }
 
+
+
+
+
+//Input:
+//use "scanf()" to get input from user
+//we also have different cases for getting input of different types
+//Integer Format Specifier: scan("%d", &age); 
+//Real Number Format Specifier: scan("%f", &pi); 
+//Char Format Specifier: scan("%c", &star); 
+
+
+#include<stdio.h> 
+int main() {
+
+  int age;
+  printf("Enter age: ");
+  scanf("%d", &age);
+  printf("age is: %d\n", age);
+
+  return 0;
+}
+
