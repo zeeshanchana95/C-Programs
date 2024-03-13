d48f33d04672c05f5a8f59bdad1254aa09f90432
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-13 10:38:16 +0500

    add the use of variables in c

--- Hello.c
+++ Hello.c
@@ -1,13 +1,48 @@
+//Variables:
+    //variable is the name of a memory location which stores some data.
+
 #include<stdio.h>
 
 int main() {
-    printf("Hello world");
+    //declare variable named age 
+    //(Syntax: datatype nameOfVariable;)
+    int age;
+
+    //initialize variable named age
+    //(Syntax: nameOfVariable = value;)
+    age = 24;
+
+    //declare and initialize variable named color at the same time
+    //(Syntax: datatype nameOfVariable = value;)
+    string color = "white";
+
+    //declaring variable with other data types
+    int number = 25;
+    char star = '*';
+    float height = 1.75;
+
+    //update existing variable value
+    age = 24;
+   
+    //Rules for Writing Variable Names:
+        // a. Variables are case sensitive
+            int a = 30;
+            int A = 40;
+
+        // b. 1st character is alphabet or '_'
+            int _age = 22;
+        
+        // c. no comma/blank space
+            int final_price = 100;
+        
+        // d. No other symbol other than
+            // int final-price = 23; //error
+
+
+    //Variable names should be meaningful and unique within the program
+    int final_price = 100;
+
+
 
     return 0;
 }
-
-//Instructions for Running the Program:
-    //1. open terminal
-    //2. move to the location where the programs are located
-    //3. to compile the program, run the commmand: gcc filname.c
-    //4. to execute the program, run the command: ./a.exe if on windows and .\a.out if on mac or linux
--- Whole-Code.c
+++ Whole-Code.c
@@ -9,13 +9,61 @@
 
     
 
-//1. Hello World Program
+//1. Hello World Program: use of printf 
 #include<stdio.h>
 
 int main() {
+    //printf() used to print the string enclosed in ""
     printf("Hello world");
 
     return 0;
 }
 
-//Variables
+//Variables:
+    //variable is the name of a memory location which stores some data.
+
+#include<stdio.h>
+
+int main() {
+    //declare variable named age 
+    //(Syntax: datatype nameOfVariable;)
+    int age;
+
+    //initialize variable named age
+    //(Syntax: nameOfVariable = value;)
+    age = 24;
+
+    //declare and initialize variable named color at the same time
+    //(Syntax: datatype nameOfVariable = value;)
+    string color = "white";
+
+    //declaring variable with other data types
+    int number = 25;
+    char star = '*';
+    float height = 1.75;
+
+    //update existing variable value
+    age = 24;
+   
+    //Rules for Writing Variable Names:
+        // a. Variables are case sensitive
+            int a = 30;
+            int A = 40;
+
+        // b. 1st character is alphabet or '_'
+            int _age = 22;
+        
+        // c. no comma/blank space
+            int final_price = 100;
+        
+        // d. No other symbol other than
+            // int final-price = 23; //error
+
+
+    //Variable names should be meaningful and unique within the program
+    int final_price = 100;
+
+
+
+    return 0;
+}
