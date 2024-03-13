6b32345c734dbc3896b083c835ea45fe2bc49b9f
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-13 19:52:19 +0500

    add c program in c

--- Hello.c
+++ Hello.c
@@ -1,45 +1,25 @@
-//Keywords:
-//these are reserved words that have special meaning to the compiler and the meaning of these words can't be changed
-//32 keywords in c 
-//can't be used for variable names
-  // auto
-  // break
-  // case
-  // char
-  // continue
-  // do
-  // default
-  // const
-  // double
-  // else
-  // enum
-  // extern
-  // for
-  // if
-  // goto
-  // float
-  // int
-  // long
-  // register
-  // return
-  // signed
-  // static
-  // sizeof
-  // short
-  // struct
-  // switch
-  // typedef
-  // union
-  // void
-  // while
-  // volatile
-  // unsigned
+//Program Structure:
 
 
-#include<stdio.h>
+#include<stdio.h> //pre-processor directive
+//always need to add above line 
+
+//"main" is the main fuction, int is the return value type that main function will return
+
+//function basically helps us to perform some task
+
+//code inside "{}" is be executed when the function main is called
+
+//every C program's starting point will be "main" function
+
+// ; semicolon indicate the end of instruction
+
+//C is a case-sensitive languages meaning "main" and "Main" are different things
 
 int main() {
 
-    
-    return 0;
+  printf("Hello World!");
+
+  return 0;
 }
+
--- Whole-Code.c
+++ Whole-Code.c
@@ -167,3 +167,31 @@ int main() {
     
     return 0;
 }
+
+
+
+//Program Structure:
+
+
+#include<stdio.h> //pre-processor directive
+//always need to add above line 
+
+//"main" is the main fuction, int is the return value type that main function will return
+
+//function basically helps us to perform some task
+
+//code inside "{}" is be executed when the function main is called
+
+//every C program's starting point will be "main" function
+
+// ; semicolon indicate the end of instruction
+
+//C is a case-sensitive languages meaning "main" and "Main" are different things
+
+int main() {
+
+  printf("Hello World!");
+
+  return 0;
+}
+
