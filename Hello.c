c71248c0e28b15e494247949251e60350f878899
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-13 11:00:38 +0500

    add the use of constants in c

--- Hello.c
+++ Hello.c
@@ -1,28 +1,15 @@
-//
-    //data type tells which type of data you store in the memory
-    //each data type specify the type of value to be stored and the memory size it will take or occupy in memory
-    //there are many data types
-        //  DATA TYPE (SIZE IN BYTES)
-        // Char or signed char (1)
-        // Unsigned char (1)
-        // int or signed int (2)
-        // Unsigned int (2)
-        // Short int or Unsigned short int (2)
-        // Signed short int (2)
-        // Long int or Signed long int (4)
-        // Unsigned long int (4)
-        // float (4)
-        // double (8)
-        // Long double (8)
-    //C LANGUAGE doesn't support boolean or string data type because I was came very early time, then c++, java, javascript, e#include<stdio.h>
+  //LETS have a look at constants
+    //constants are values that don't change (fixed)
+        // we have 
+            //integer constants: 1,2,3,0,-1,-2
+
+            //real constants: 1.0, 2.0, 3.14, -2.4
+            
+            //character constants (need to be enclosed in single quotes): 'a', 'b', 'A', '#', '&'
+
+#include<stdio.h>
 
 int main() {
-
-    //le'ts have a look at the most common data types
-
-    int age = 22; //for storing whole numbers
-    float pi = 3.142; //for storing values with decimal point
-    char hashtag = '#'; //for storing special characters
 
     return 0;
 }
--- Whole-Code.c
+++ Whole-Code.c
@@ -100,3 +100,22 @@ int main() {
 
     return 0;
 }
+
+
+
+//Contants:
+  //LETS have a look at constants
+    //constants are values that don't change (fixed)
+        // we have 
+            //integer constants: 1,2,3,0,-1,-2
+
+            //real constants: 1.0, 2.0, 3.14, -2.4
+            
+            //character constants (need to be enclosed in single quotes): 'a', 'b', 'A', '#', '&'
+
+#include<stdio.h>
+
+int main() {
+
+    return 0;
+}
