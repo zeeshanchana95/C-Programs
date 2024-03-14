54d51450ab183afb08350052ddd388edb441278b
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-14 12:01:12 +0500

    add instructions with types

--- Hello.c
+++ Hello.c
@@ -1,26 +1,14 @@
-// Practice Qs 2
-// Write a program to calculate area of a circle.
-// (side is given)
+// Control Instructions
+  // Used to determine flow of program
+  // a. Sequence Control
+  // b. Decision Control
+  // c. Loop Control
+  // d. Case Control
 
-#include<stdio.h>
+#include <stdio.h>
+
 int main() {
 
-  float radius;
-  printf("Enter radius: ");
-  scanf("%f", &radius);
-  printf("Area of circle is: %f", 3.142*radius*radius);
-
+    
   return 0;
 }
-
-// HOMEWORK SET
-// a. Write a program to calculate perimeter of rectangle.
-// Take sides, a & b, from the user.
-// b. Take a number(n) from user & output its cube(n*n*n).
-// c. Write comments for programs a & b.
-// d. temperature conversion from fahrenheit to celsius or vice versa.
-// e. calculate interest using given formula
-// f. calculate compound interest.
-// g. calculate area of triangle, rectangle, etc.
-
-
--- Whole-Code.c
+++ Whole-Code.c
@@ -1,6 +1,22 @@
 
 //C++ Tutorials:
 
+
+//======================= UNIT 1: Basic Concepts =====================
+//Basic Program
+//printf()
+//Variables
+//Keywords
+//Data Types
+//Constants
+//Comments
+//Input and Output Statements
+//Format Specifiers
+//Program Structure
+//Practice Questions
+
+
+
 //Instructions for Running the Program:
     //1. open terminal
     //2. move to the location where the programs are located
@@ -400,3 +416,269 @@ int main() {
 // e. calculate interest using given formula
 // f. calculate compound interest.
 // g. calculate area of triangle, rectangle, etc.
+
+
+//======================= END of UNIT 1: Basic Concepts =====================
+
+
+
+//============ UNIT 02: Instruction and Operators ===================
+//1. What are Instructions in C, and its Types?
+
+
+
+
+//Instructions:
+  //these are statements in the program
+  //they are of three types: all of them have different functions to do and that's how they named accordingly
+    //1. Type Declaration Instructions
+    //2. Arithmetic Instructions
+    //3. Control Instructions
+
+//1. type declaration instructions
+  //instructions where there is a need to declaring variables with its types i.e., int a = 2;
+  //there is a RULE while declaring instructions: DECLARE VARIABLE BEFORE USING IT.
+
+//Examples:
+  // VALID
+  // int a = 22;
+  // int b = a;
+  // int c=b+l;
+  // int d=l,e;
+
+  // int a,b,c;
+  // a=b=c = 1
+
+  // INVALID
+  // int a = 22
+  // int b = a
+  // int c= b +2
+  // int d=2, e
+
+
+#include <stdio.h>
+int main() {
+
+  int a = 22;
+  int b = a;
+  int c = b * 6;
+  int d = 1, e;
+
+  int oldAge = 22;
+  int years = 2;
+  int newAge = oldAge + years;
+
+
+  //if declaring multiple variables, you need to first declare them all and then initialize them at a time
+
+  //correct
+  int a,b,c;
+  a = b = c = 1;
+  
+  // int x = y = z = 4; //error
+
+
+
+  return 0;
+
+}
+
+
+
+
+//2. Arithmetic Instructions (+, -, *, /, %)
+  //instructions which has operands and operators are called arithmetic operations
+
+  //we have "operands -> values on which operations being performed like a = 2" and "operators -> operation to perform like +"
+  
+  //NOTE: there must a single variable on the Left Hand Side to get the result of operation being performed becuase variables on the left are being assigned by values from the right side 
+
+
+  
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+  int a = 1, b = 2;
+
+  int sum = a + b;
+  int multiply = a * b;
+  int x,y = a*b; //NOTE: y assigned value but x is declared only
+
+
+  //valid instructions:
+  a = b + c;
+  a = b * c;
+  a = b / c;
+
+
+  //invalid instructions
+  b + c = a;
+  a = bc;
+  a = b ^ c; //^ is used for XOR operation (bitwise operation)
+
+//NOTE: pow(x,y) for x to the power of y
+//pow(b,c) = b^c
+printf("%d", a);
+
+
+
+//Modular Operator: 
+  //return remainder for int 
+  //use % for mudular operator
+
+  //two cases for integer values:
+    //positive numerator: return positive remainder
+    //negative numerator: return negative remainder
+
+  //Examples:
+  // 3 % 2 = 1
+  // -3 % 2 = -1
+  // 4 % 2 = 0;
+  //5 % 3 = 2;
+  
+  printf("%d", 16%10); //return 6
+
+  // modular opertor doesn't work on float values
+  printf("%d", 1.6%1.0); //error: 
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2. Arithmetic Instructions
+
+  //TYPE CONVERSION:
+    //if operands are of same type -> get result of same type
+      // int op int -> int (2 * 2 -> 4)
+      // float op float -> float (2.0 * 2.0 -> 4.0)
+
+    //if operands are of differnet type -> get result of differnet type
+
+      // int op float -> float (2.0 * 2 -> 4.0)
+      // float op double -> double
+
+
+    //Practic Question:
+      //Solve:
+      int a = 1.999999; //Warning: implicit conversion from double to int
+
+    //Basically there are 2 types of conversions:
+    //1. Implicit conversion
+      //- do compiler itself
+      //says smaller types can be converted into bigger types like
+        //int -> int
+        //int -> double
+        //BUT
+        //float -> int (not possible automatically)
+        //double -> int (not possible automatically)
+        
+        //we need to tell the compiler to convert explicitly, like
+          //int a = (int) 1.999999;
+
+
+
+    //2. Explicit conversion
+      //- do by ourselves but not by compiler
+
+  //OPERATOR PRECEDENCE:
+    //precedence means priority
+    //one at the top has higher precedence than the other at the bottom
+    //higher precedence means higher priority
+    
+      // *, /, %
+      // +, -
+      // = 
+      
+      //Example: Operation of Different Precedence
+        // //correct
+        // x = 4 + 9 * 10;
+        // x = 4 + 90
+        // x = 94
+
+        // //wrong
+        // x = 4 + 9 * 10;
+        // x = 13 * 10;
+        // x = 130
+
+      //Example: Operation of Same Precedence
+        //come associativily rules for same precedence
+          //perform operations from left to right
+            // x = 4 * 3 / 6 * 2;
+            // x = 12 / 6 * 2;
+            // x = 2 * 2
+            // x = 4
+
+            //OR
+             //x = ((4 * 3) / 6) * 2
+
+    //Practice Qs 4
+      //a. 5 * 2 - 2 * 3  => 10 - 6 => 4
+
+
+      //b. 5 * 2 / 2*3 => 10 / 2 * 3 => 5 * 3 => 15
+      
+      
+      //c. 5 * (2 / 2) * 3 => 5 * (1) * 3 => 5 * 1 * 3 => 5 * 3 => 15
+
+
+      //d. 5 + 2 / 2 * 3 => (5 + ((2/2) * 3)) =>  5 + ((1) * 3) => 5 + 3 => 8
+
+
+
+
+
+
+
+#include <stdio.h>
+int main() {
+
+  // printf("%d\n", 2 * 2); //4
+  // printf("%f\n", 2.0 * 2.0); //4.0
+
+  // printf("%f\n", 2.0 * 2); //4.0000000
+
+  // printf("%d\n", 2 / 3); //0 becuase int (2) op int (3) -> int (0.666667 -> 0)
+
+  // int a = 1.999999; 
+  // printf("%d\n", a); //warning
+
+  // int b = (int) 1.999999; //forcefully convert to int
+  // printf("%d\n", b); //1
+
+  // int a = 4 * 3 / 6 * 2;
+  // printf("%d\n", a);
+  
+  // int a = 5 * 2 - 2 * 3;
+  // printf("%d\n", a);
+
+  // int a = 5 * 2 / 2*3;
+  // printf("%d\n", a);
+
+  // int a =  5 * (2 / 2) * 3;
+  // printf("%d\n", a);
+
+  int a =   5 + 2 / 2 * 3;
+  printf("%d\n", a);
+
+  return 0;
+
+}
