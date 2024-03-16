0eaed7dbd3f46999106aaaa8e37ff30be8f0497e
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-16 20:01:49 +0500

    add practice question solution

--- Hello.c
+++ Hello.c
@@ -1,18 +1,25 @@
-// Practice Qs IO
-// Write a Program to give grades to a student
-// marks < 30 is C
-// 30 <= marks < 70 is B
-// 70<= marks < 90 is A
-// 90 marks 100 is A+
+
+
+// Practice Qs 1 1
+// Will this code :
+// a. give error
+// b. print "x is equal to 1"
+// c. print "x is not equal to 1 "
+
 
 #include <stdio.h>
 
 int main() {
 
-
-
+  int x = 2;
+  if(x=4) {
+    printf("x is equal to 1\n");
+    printf("%d \n", x);
+  }
+  else {
+    printf("x is not equal to 1");
+  }
+  
   return 0;
-
-
 
 }
--- Whole-Code.c
+++ Whole-Code.c
@@ -1454,11 +1454,65 @@ int main() {
   marks >= 0 && marks <= 30 ? printf("Fail") 
   : marks > 30 && marks <= 100 ? printf("Pass") 
   : printf("Invalid Marks");
-  
 
   return 0;
-
-
-
 }
 
+
+
+
+
+
+// Practice Qs IO
+// Write a Program to give grades to a student
+// marks < 30 is C
+// 30 <= marks < 70 is B
+// 70<= marks < 90 is A
+// 90 marks 100 is A+
+
+#include <stdio.h>
+
+int main() {
+
+  int marks;
+  printf("Enter your marks: ");
+  scanf("%d", &marks);
+  if (marks < 30) {
+    printf("C \n");
+  } else if( marks >= 30 && marks < 70){
+    printf("B \n");
+  } else if (marks >= 70 && marks <= 90) {
+    printf("A \n");
+  } else  {
+    printf("A+ \n");
+  }
+
+  return 0;
+}
+
+
+
+
+// Practice Qs 1 1
+// Will this code :
+// a. give error
+// b. print "x is equal to 1"
+// c. print "x is not equal to 1 "
+
+
+#include <stdio.h>
+
+int main() {
+
+  int x = 2;
+  if(x=4) {
+    printf("x is equal to 1\n");
+    printf("%d \n", x);
+  }
+  else {
+    printf("x is not equal to 1");
+  }
+  
+  return 0;
+
+}
