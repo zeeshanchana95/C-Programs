df3defd0b7ba7a10e7b0f9d141d7d6a75e08c0d8
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-16 20:09:27 +0500

    add practice question 12 and homework questions

--- Hello.c
+++ Hello.c
@@ -1,25 +1,30 @@
-
-
-// Practice Qs 1 1
-// Will this code :
-// a. give error
-// b. print "x is equal to 1"
-// c. print "x is not equal to 1 "
-
+// Practice Qs 12
+// Write a Program to find if a character entered by user is uppercase or not.
 
 #include <stdio.h>
 
 int main() {
 
-  int x = 2;
-  if(x=4) {
-    printf("x is equal to 1\n");
-    printf("%d \n", x);
-  }
-  else {
-    printf("x is not equal to 1");
-  }
-  
+ char character;
+ printf("Enter character: ");
+ scanf("%c", &character);
+
+ if(character >= 'A' && character <= 'Z') {
+  printf("Uppercase character");
+ } else if(character >= 'a' && character <= 'z') {
+  printf("Lowercase character");
+ } else {
+  printf("not english alphabet character");
+ }
   return 0;
 
 }
+
+/*
+HOMEWORK SET
+  a. Write a program to check if a given number is
+  Armstrong number or not.
+  (Search what is Armstrong number)
+  b. Write a program to check if the given number is a natural number.
+  (Natural numbers start from 1)
+*/
--- Whole-Code.c
+++ Whole-Code.c
@@ -1516,3 +1516,35 @@ int main() {
   return 0;
 
 }
+
+
+// Practice Qs 12
+// Write a Program to find if a character entered by user is uppercase or not.
+
+#include <stdio.h>
+
+int main() {
+
+ char character;
+ printf("Enter character: ");
+ scanf("%c", &character);
+
+ if(character >= 'A' && character <= 'Z') {
+  printf("Uppercase character");
+ } else if(character >= 'a' && character <= 'z') {
+  printf("Lowercase character");
+ } else {
+  printf("not english alphabet character");
+ }
+  return 0;
+
+}
+
+/*
+HOMEWORK SET
+  a. Write a program to check if a given number is
+  Armstrong number or not.
+  (Search what is Armstrong number)
+  b. Write a program to check if the given number is a natural number.
+  (Natural numbers start from 1)
+*/
