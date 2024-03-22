6a93232a65da1a9e382f5d59bd81f5c417543be8
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-03-22 10:22:58 +0500

    add code for practice questions in struct

--- Hello.c
+++ Hello.c
@@ -1,27 +1,65 @@
 // Chapter 09: Structures
 
-    // typedef Keyword
-    
-    // used to create alias for data types
-    //     typedef struct ComputerEngineeringStudent{
-    //     int roll;
-    //     float cgpa;
-    //     char name [100];
-    //     } coe;
-    //     coe student1 ;
+    // Practice Qs 57
+        // Create a structure to store vectors. Then make a function to return sum of 2 vectors.
+
 
 #include<stdio.h>
+#include<string.h>
 
-struct student {
-    int roll;
-    float cgpa;
-    char name[100];
+
+struct address {
+    int houseNo;
+    int block;
+    char city[100];
+    char state[100];
 };
 
+void printAddress(struct address add);
 
 int main () {
 
-   
+    struct address adds[5];
+    //input
+    printf("Enter info for person 1: ");
+    scanf("%d", &adds[0].houseNo);
+    scanf("%d", &adds[0].block);
+    scanf("%s", adds[0].city);
+    scanf("%s", adds[0].state);
+
+    printf("Enter info for person 2: ");
+    scanf("%d", &adds[1].houseNo);
+    scanf("%d", &adds[1].block);
+    scanf("%s", adds[1].city);
+    scanf("%s", adds[1].state);
+
+    printf("Enter info for person 3: ");
+    scanf("%d", &adds[2].houseNo);
+    scanf("%d", &adds[2].block);
+    scanf("%s", adds[2].city);
+    scanf("%s", adds[2].state);
+
+    printf("Enter info for person 4: ");
+    scanf("%d", &adds[3].houseNo);
+    scanf("%d", &adds[3].block);
+    scanf("%s", adds[3].city);
+    scanf("%s", adds[3].state);
+
+    printf("Enter info for person 5: ");
+    scanf("%d", &adds[4].houseNo);
+    scanf("%d", &adds[4].block);
+    scanf("%s", adds[4].city);
+    scanf("%s", adds[4].state);
+
+    printAddress(adds[0]);
+    printAddress(adds[1]);
+    printAddress(adds[2]);
+    printAddress(adds[3]);
+    printAddress(adds[4]);
 
     return 0;
+}
+
+void printAddress(struct address add) {
+    printf("address is: %d, %d, %s, %s\n", add.houseNo, add.block, add.city, add.state);
 }
